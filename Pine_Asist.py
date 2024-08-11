541f2a4a53dce96cd9ef358dafa092f48d093e14
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-11 12:15:11 -0500

    Update Pine_Asist.py

--- Pine_Asist.py
+++ Pine_Asist.py
@@ -16,7 +16,7 @@ if os.environ['OPENAI_API_KEY']:
     #st.set_page_config('preguntaDOC')
     pinecone.init(
     api_key=ke,
-    environment= "https://us-east-1"
+    environment= "https://pdfdb-0em6vtx.svc.aped-4627-b74a.pinecone.io"
     )
     st.header("Pregunta a tu PDF")
     
