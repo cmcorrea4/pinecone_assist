29fa78f04a2e5a8f5437212bcbab7b6e49444e54
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-11 11:34:34 -0500

    Update Pine_Asist.py

--- Pine_Asist.py
+++ Pine_Asist.py
@@ -11,52 +11,51 @@ FILE_LIST = "archivos.txt"
 
 
 ke = st.text_input('Ingresa tu Clave')
-#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 os.environ['OPENAI_API_KEY'] = ke
-
-st.set_page_config('preguntaDOC')
-st.header("Pregunta a tu PDF")
-
-with st.sidebar:
-    archivos = load_name_files(FILE_LIST)
-    files_uploaded = st.file_uploader(
-        "Carga tu archivo",
-        type="pdf",
-        accept_multiple_files=True
-        )
+if os.environ['OPENAI_API_KEY']:
+    st.set_page_config('preguntaDOC')
+    st.header("Pregunta a tu PDF")
     
-    if st.button('Procesar'):
-        for pdf in files_uploaded:
-            if pdf is not None and pdf.name not in archivos:
-                archivos.append(pdf.name)
-                text_to_pinecone(pdf)
-
-        archivos = save_name_files(FILE_LIST, archivos)
-
-    if len(archivos)>0:
-        st.write('Archivos Cargados:')
-        lista_documentos = st.empty()
-        with lista_documentos.container():
-            for arch in archivos:
-                st.write(arch)
-            if st.button('Borrar Documentos'):
-                archivos = []
-                clean_files(FILE_LIST)
-                lista_documentos.empty()
-
-
-if len(archivos)>0:
-    user_question = st.text_input("Pregunta: ")
-    if user_question:
-        os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-        embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    with st.sidebar:
+        archivos = load_name_files(FILE_LIST)
+        files_uploaded = st.file_uploader(
+            "Carga tu archivo",
+            type="pdf",
+            accept_multiple_files=True
             )
-        vstore = Pinecone.from_existing_index(INDEX_NAME, embeddings)
-
-        docs = vstore.similarity_search(user_question, 3)
-        llm = ChatOpenAI(model_name='gpt-3.5-turbo')
-        chain = load_qa_chain(llm, chain_type="stuff")
-        respuesta = chain.run(input_documents=docs, question=user_question)
-
-        st.write(respuesta)
+        
+        if st.button('Procesar'):
+            for pdf in files_uploaded:
+                if pdf is not None and pdf.name not in archivos:
+                    archivos.append(pdf.name)
+                    text_to_pinecone(pdf)
+    
+            archivos = save_name_files(FILE_LIST, archivos)
+    
+        if len(archivos)>0:
+            st.write('Archivos Cargados:')
+            lista_documentos = st.empty()
+            with lista_documentos.container():
+                for arch in archivos:
+                    st.write(arch)
+                if st.button('Borrar Documentos'):
+                    archivos = []
+                    clean_files(FILE_LIST)
+                    lista_documentos.empty()
+    
+    
+    if len(archivos)>0:
+        user_question = st.text_input("Pregunta: ")
+        if user_question:
+            os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+            embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+                )
+            vstore = Pinecone.from_existing_index(INDEX_NAME, embeddings)
+    
+            docs = vstore.similarity_search(user_question, 3)
+            llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+            chain = load_qa_chain(llm, chain_type="stuff")
+            respuesta = chain.run(input_documents=docs, question=user_question)
+    
+            st.write(respuesta)
