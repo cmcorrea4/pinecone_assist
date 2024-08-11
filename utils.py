6819be5b2e70171ebce6546432b2d91dae60a5ef
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-11 11:45:51 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,8 +16,8 @@ PINECONE_ENV = "Añadir Pinecone Env"
 INDEX_NAME = 'taller'
 
 pinecone.init(
-    api_key=PINECONE_API_KEY,
-    environment=PINECONE_ENV
+    api_key=ke,
+    environment=us-east-1
 )
 
 
